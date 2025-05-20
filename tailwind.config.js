ee0281d933b94fae64bea3e53a3adf2baf16bd78
Gustavo <maiagustavo125@gmail.com>
2025-05-20 20:08:18 -0300

    chore(tema): Cores do site como padrão no Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#08C952",
+        secundary: "#160440",
+        box: "#808080",
+        box_1: "#7B7B7B",
+        background: "#f2f2f2"
+      }
+    },
+  },
+  plugins: [],
+};
