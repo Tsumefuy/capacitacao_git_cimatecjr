2280d978ffe71cf8453b11d36b7317da38ff8f60
Gustavo <maiagustavo125@gmail.com>
2025-05-20 23:19:09 -0300

    fix: Header background

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -16,14 +16,14 @@ export function Header() {
     ];
 
     return (
-        <header className="absolute left-0 w-full h-20 flex items-center justify-between px-6 md:px-20 py-4 bg-background border-b border-black z-50">
+        <header className="absolute left-0 w-full h-20 flex items-center justify-between px-6 md:px-20 py-4 bg-color border-b border-black z-50">
             <Link href="/" className="flex items-center text-2xl">
                 <Image src="/icon.png" alt="Portal Gun" className="mr-3 mb-1" width={48} height={48} />
                 <span className="text-primary font-extrabold">PortalGun</span>
                 <span className="text-secondary font-normal">Store</span>
             </Link>
             <nav className={`md:flex items-center space-x-8 md:space-x-12 text-secundary text-base font-normal 
-                ${openMenu ? "block absolute top-20 right-0 w-full transform transition-all duration-300 ease-in-out bg-background shadow-md p-4" : "hidden"} `}>
+                ${openMenu ? "block absolute top-20 right-0 w-full transform transition-all duration-300 ease-in-out bg-color shadow-md p-4" : "hidden"} `}>
                 {navLinks.map((link) => (
                     <Link key={link.path} href={link.path} className="relative group hover:text-gray-500 transition-all duration-300">
                         {link.name}
