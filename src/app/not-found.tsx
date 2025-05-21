4b8a90c9fdd536fbe43a5650e324643af87ceb80
Gustavo <maiagustavo125@gmail.com>
2025-05-20 23:43:33 -0300

    feat: Página Not-Found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-center font-bold mt-9 text-3xl">Puts, uma página 404!</h1>
+      <p>Essa página não existe!</p>
+    </div>
+  );
+}
